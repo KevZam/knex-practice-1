67fd03c9d24447ff43537f77b40fcef4e922d9ef
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-02-25 12:36:40 -0800

    added getById() functionality

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -2,11 +2,31 @@ const shoppingListService = {
   // Read (get)
   getAllItems(knex) {
     return knex.select("*").from("shopping_list");
-  }
+  },
   // Create
-
+  insertItem(knex, newItem) {
+    return knex
+      .insert(newItem)
+      .into("shopping_list")
+      .returning("*")
+      .then(rows => {
+        return rows[0];
+      });
+  },
+  // Get by ID
+  getByID(knex, itemID) {
+    return knex
+      .select("*")
+      .from("shopping_list")
+      .where("id", itemID)
+      .first();
+  },
   // Update
-
+  updateItem(knex, id, newItemInfo) {
+    return knex("shopping_list")
+      .where({ id })
+      .update(newItemInfo);
+  }
   // Delete
 };
 
--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -54,5 +54,68 @@ describe("Shopping list service object", () => {
         expect(actual).to.eql(testProducts);
       });
     });
+
+    it("returns the id of a given item", () => {
+      thirdId = 3;
+      thirdItem = testProducts[thirdId - 1];
+
+      return shoppingListService.getByID(db, thirdId).then(actual => {
+        expect(actual).to.eql({
+          id: thirdId,
+          name: thirdItem.name,
+          price: thirdItem.price,
+          date_added: thirdItem.date_added,
+          checked: thirdItem.checked,
+          category: thirdItem.category
+        });
+      });
+    });
+
+    it("updates the item by a given id", () => {
+      const idOfItemToUpdate = 3;
+      const newInfo = {
+        id: 3,
+        name: "Salad",
+        price: "2.50",
+        date_added: new Date("2020-03-02T01:25:44.848Z"),
+        checked: false,
+        category: "Lunch"
+      };
+
+      return shoppingListService
+        .updateItem(db, idOfItemToUpdate, newInfo)
+        .then(() => shoppingListService.getByID(db, idOfItemToUpdate))
+        .then(item => {
+          console.log(item);
+          expect(item).to.eql({
+            id: idOfItemToUpdate,
+            ...newInfo
+          });
+        });
+    });
+  });
+
+  context("Given that the shoppin_list has no items", () => {
+    it("should return an empty array", () => {
+      return shoppingListService.getAllItems(db).then(actual => {
+        expect(actual).to.eql([]);
+      });
+    });
+
+    it("insertItem() should insert a new item and resolve it with an ID", () => {
+      let newItem = {
+        name: "NewItem",
+        price: "2.50",
+        date_added: new Date("2020-05-02T01:25:44.848Z"),
+        checked: true,
+        category: "Lunch"
+      };
+      return shoppingListService.insertItem(db, newItem).then(actual => {
+        expect(actual).to.eql({
+          id: 1,
+          ...newItem
+        });
+      });
+    });
   });
 });
